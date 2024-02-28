79da0c442a26f11c9d31cc39a5ea541dac63b206
Magaca78 <mateo.1701920666@ucaldas.edu.co>
2024-02-27 20:27:15 -0500

    Creacion de submenus1 y submenus2

--- main.py
+++ main.py
@@ -2,28 +2,60 @@ import streamlit as st
 
 def menu_principal():
     st.title("Menú Principal")
-    opciones = ["Inicio", "Configuración", "Ayuda"]
+    opciones = ["Archivo", "Editar", "Ejecutar", "Herramientas", "Ventana","Ayuda"]
     seleccion = st.sidebar.selectbox("Selecciona una opción", opciones)
 
-    if seleccion == "Inicio":
-        mostrar_inicio()
-    elif seleccion == "Configuración":
-        mostrar_configuracion()
+    if seleccion == "Archivo":
+        archivo()
+    elif seleccion == "Editar":
+        editar()
+    elif seleccion == "Ejecutar":
+        ejecutar()
+    elif seleccion == "Herramientas":
+        ejecutar()
+    elif seleccion == "Ventana":
+        ejecutar()
     elif seleccion == "Ayuda":
-        mostrar_ayuda()
+        ejecutar()
 
-def mostrar_inicio():
-    st.header("Página de Inicio")
-    st.write("Bienvenido a la aplicación. Esta es la página de inicio.")
+def archivo():
+    st.header("Página de Archivo")
+    st.write("Bienvenido a la aplicación. Esta es la página de archivo.")
+    submenu_opcion = st.sidebar.selectbox("Seleccione una opción", 
+                                          ["Nuevo Grafo", "Abrir", "Cerrar", 
+                                           "Guardar", "Guardar Como", "Exportar datos",
+                                           "Importar datos", "Salir"])
+    
 
-def mostrar_configuracion():
-    st.header("Página de Configuración")
-    st.write("Aquí puedes ajustar la configuración de la aplicación.")
+def editar():
+    st.header("Página de editar")
+    st.write("Bienvenido a la aplicación. Esta es la página de editar.")
+    submenu_opcion = st.sidebar.selectbox("Seleccione una opción", 
+                                          ["Deshacer", "Nodo", "Arco" ])
 
-def mostrar_ayuda():
-    st.header("Página de Ayuda")
-    st.write("Si necesitas ayuda, consulta la documentación o ponte en contacto con nosotros.")
+def ejecutar():
+    st.header("Página de ejecutar")
+    st.write("Bienvenido a la aplicación. Esta es la página de ejecutar.")
+    submenu_opcion = st.sidebar.selectbox("Seleccione una opción", 
+                                          ["Procesos"])
+    
+def herramientas():
+    st.header("Página de herramientas")
+    st.write("Bienvenido a la aplicación. Esta es la página de herramientas.")
+    submenu_opcion = st.sidebar.selectbox("Seleccione una opción", 
+                                          ["....Coming soon"])
+    
+def ventana():
+    st.header("Página de ventana")
+    st.write("Bienvenido a la aplicación. Esta es la página de ventana.")
+    submenu_opcion = st.sidebar.selectbox("Seleccione una opción", 
+                                          ["Grafica", "Tabla"])
+    
+def ayuda():
+    st.header("Página de ayuda")
+    st.write("Bienvenido a la aplicación. Esta es la página de ayuda.")
+    submenu_opcion = st.sidebar.selectbox("Seleccione una opción", 
+                                          ["Ayuda", "Acerca de grafos"])
 
 # Llama a la función del menú principal
 menu_principal()
-
