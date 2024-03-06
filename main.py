3a1fb520acdd364197b681a25917fbc7caaa3650
juanmz1606 <juan.1701920466@ucaldas.edu.co>
2024-03-05 22:05:26 -0500

    LectorJSON

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import streamlit as st
+from streamlit_agraph import agraph, Node, Edge, Config
+import json
+import LectorJSON as Ljson
 
 def menu_principal():
     st.title("Menú Principal")
@@ -12,11 +15,11 @@ def menu_principal():
     elif seleccion == "Ejecutar":
         ejecutar()
     elif seleccion == "Herramientas":
-        ejecutar()
+        herramientas()
     elif seleccion == "Ventana":
-        ejecutar()
+        ventana()
     elif seleccion == "Ayuda":
-        ejecutar()
+        ayuda()
 
 def archivo():
     st.header("Página de Archivo")
@@ -25,6 +28,10 @@ def archivo():
                                           ["Nuevo Grafo", "Abrir", "Cerrar", 
                                            "Guardar", "Guardar Como", "Exportar datos",
                                            "Importar datos", "Salir"])
+    if submenu_opcion == "Abrir":
+        archivo_Abrir()
+    elif submenu_opcion == "Nuevo Grafo":
+        archivo_NuevoGrafo()
     
 
 def editar():
@@ -56,6 +63,39 @@ def ayuda():
     st.write("Bienvenido a la aplicación. Esta es la página de ayuda.")
     submenu_opcion = st.sidebar.selectbox("Seleccione una opción", 
                                           ["Ayuda", "Acerca de grafos"])
+    
+    
+def archivo_NuevoGrafo():
+    nodes = [
+        Node(id="Spiderman", label="Peter Parker", size=25, shape="circularImage", image="http://marvel-force-chart.surge.sh/marvel_force_chart_img/top_spiderman.png"),
+        Node(id="Captain_Marvel", size=25, shape="circularImage", image="http://marvel-force-chart.surge.sh/marvel_force_chart_img/top_captainmarvel.png")
+    ]
+
+    edges = [
+        Edge(source="Captain_Marvel", label="friend_of", target="Spiderman")
+    ]
+
+    config = Config(width=750, height=950, directed=True, physics=True, hierarchical=False)
+
+    # Mostrar el grafo utilizando agraph
+    return_value = agraph(nodes=nodes, edges=edges, config=config)
+    st.write(return_value)
+
+def archivo_Abrir():
+
+    # Widget for uploading the JSON file
+    uploaded_file = st.file_uploader("Select a JSON file", type=["json"])
+
+    if uploaded_file is not None:
+        try:
+            # Read the uploaded file
+            json_data = json.load(uploaded_file)
+            return_value = Ljson(json_data)
+            st.write(return_value)
+            
+        except json.JSONDecodeError:
+            st.error("Error al decodificar el archivo JSON. Asegúrate de que el archivo tenga un formato JSON válido.")
+
 
 # Llama a la función del menú principal
 menu_principal()
